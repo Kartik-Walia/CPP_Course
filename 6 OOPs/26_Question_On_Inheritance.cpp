c37b3973cf2107b84d2ca16ccc321c83c2631393
Kartik-Walia <itzkartikeds3@gmail.com>
2023-11-06 11:39:06 +0530

    Ambiguity Resolution in Inheritance

---
+++ 6 OOPs/26_Question_On_Inheritance.cpp
@@ -0,0 +1,21 @@
+/*
+QUESTION - Create 2 classes:
+1. SimpleCalculator - Takes input of 2 numbers using a utility function and performs +, -, *, / and displays the
+    result using another function.
+2. ScientificCalculator - Takes input of 2 numbers using a utility function and performs any 4 scientific
+    operation of your choice and displays the results using another function.
+
+    Create another class HybridCalculator and inherit it using these 2 classes.
+    Q1 What type of inheritance are you using ?
+    Q2 What mode of inheritance are you using ? 
+    Q3 Create an object of HybridCalculator and display results of simple and scientific calculator
+    Q4 How is code reusability implemented ?
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
